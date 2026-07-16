93d6e8a773c4406e33c17ad25722979228072278
Aaron Chavez <aaron@glyphforged.com>
2026-07-16 14:51:03 -0400

    Fix function signature for mrc_scroll_init

--- src/scroll.c
+++ src/scroll.c
@@ -8,7 +8,7 @@ static void wipe_scroll(MRC_Scroll *scroll)
   scroll -> unit_size = 0;
 }
 
-MRC_SCrollStatus mrc_scroll_init(MRC_Scroll *scroll, size_t capacity, size_t u_size)
+MRC_ScrollStatus mrc_scroll_init(MRC_Scroll *scroll, size_t capacity, size_t u_size)
 {
   // Check for immediate fail states
   if (scroll == NULL) return SCROLL_ERR_ARG_INVALID;
