7350fed877c146087ef7c629c2ba7fe51c79b507
Aaron C <aaron@glyphforged.com>
2026-07-16 22:08:49 -0400

    Added Push and Pop functions (untested)
    Wrote header signatures for insert, remove, and scan, all similar enough to push and pop, should be an easy follow-up.

--- include/mrc/scroll.h
+++ include/mrc/scroll.h
@@ -17,8 +17,8 @@ typedef struct{
 typedef enum {
   SCROLL_OK = 0,            ///< Operation completed successfully
   SCROLL_ERR_OOM,           ///< Out of memory (allocation failed)
-  SCROLL_ERR_OVERFLOW,      ///< Requestedallocation size sxceeds MAX_SIZE limit
-  SCROLL_ERR_SIZE_INVALID,  ///< Requested capacity size is out of acceptable boudns
+  SCROLL_ERR_OVERFLOW,      ///< Requested allocation size sxceeds MAX_SIZE limit
+  SCROLL_ERR_SIZE_INVALID,  ///< Requested capacity size is out of acceptable bounds
   SCROLL_ERR_ARG_INVALID,   ///< A passed argument pointer was NULL
   SCROLL_ERR_SIZE_ZERO,     ///< The unit size cannot be zero
   SCROLL_ERR_BOUNDS,        ///< Attempted to access an index out of bounds
@@ -32,7 +32,7 @@ typedef enum {
  *       On failure, the struct members are cleared or left unchanged
  *
  * @param[in,out] scroll        The address of an unitialized scroll created by the user
- * @param[in]     capacity      The starting capacity of the scroll will default to 2 if one is not provided or if the value provided is < 2
+ * @param[in]     capacity      The starting capacity will default to 2 if the value provided is < 2
  * @param[in]     unit_size     The size of a single element in bytes. Must be > 0
  *
  * @return 0 on success, or a non-zero SCROLL_ERR error code on failure
@@ -49,3 +49,74 @@ MRC_ScrollStatus mrc_scroll_init(MRC_Scroll *scroll, size_t capacity, size_t uni
  * @return 0 on success, or SCROLL_ERR_ARG_INVALID if scroll is NULL
  */
 MRC_ScrollStatus mrc_scroll_purge(MRC_Scroll *scroll);
+
+/**
+ * @brief Pushes a new element onto the end of the scroll, resizing if necessary
+ * 
+ * @pre scroll must point to an initialized MRC_Scroll struct
+ * @pre data must point to a valid memory location of size scroll->unit_size
+ * 
+ * @param[in,out]   scroll    The address of the scroll to push data onto
+ * @param[in]       data      The address of the data to be pushed onto the scroll
+ * 
+ * @return 0 on success, or a non-zero SCROLL_ERR error code on failure
+ */
+MRC_ScrollStatus mrc_scroll_push(MRC_Scroll *scroll, void *data);
+
+/**
+ * @brief Pops an element off the end of the scroll, copying the data to a destination buffer
+ * 
+ * This is a destructive operation on the scroll.
+ * 
+ * @pre scroll must point to an initialized MRC_Scroll struct
+ * @pre dest must point to a valid buffer of the appropriate size
+ * 
+ * @param[in]       scroll    The address of the scroll to pop the data from
+ * @param[in, out]  dest      The address of the destination buffer
+ * 
+ * @return 0 on success, or a non-zer SCROLL_ERR error code on failure
+ */
+MRC_ScrollStatus mrc_scroll_pop(MRC_Scroll *scroll, void *dest);
+
+/**
+ * @brief Provides a copy of the data at a given index location (non-destructive)
+ * 
+ * @pre scroll must point to an initialized MRC_Scroll struct
+ * @pre dest must point to a valid destination buffer of size scroll->unit_size
+ * 
+ * @param[in]       scroll  The address of the scroll
+ * @param[in, out]  dest    The address of a destination buffer
+ * 
+ * @return 0 on success, or a non-zero SCROLL_ERR error code on failure
+ */
+MRC_ScrollStatus mrc_scroll_scan(MRC_Scroll *scroll, void *dest);
+
+/**
+ * @brief Inserts a new element into the the scroll at a given index, resizing if necessary
+ * 
+ * @pre scroll must point to an initialized MRC_Scroll struct
+ * @pre data must point to a valid memory location of size scroll->unit_size
+ * @pre index must not be greater than span or less than zero
+ * 
+ * @param[in,out]   scroll    The address of the scroll to push data onto
+ * @param[in]       data      The address of the data to be inserted into the scroll
+ * @param[in]       index     The index where data will be inserted
+ * 
+ * @return 0 on success, or a non-zero SCROLL_ERR error code on failure
+ */
+MRC_ScrollStatus mrc_scroll_insert(MRC_Scroll *scroll, void *data, int index);
+
+/**
+ * @brief Provides a copy of data at a given location and removes it from the scroll
+ * 
+ * This is a destructive operation on the scroll
+ * 
+ * @pre scroll must point to an initialized MRC_Scroll struct
+ * @pre dest must point to a valid destination buffer of size scroll->unit_size
+ * 
+ * @param[in]       scroll  The address of the scroll
+ * @param[in, out]  dest    The address of a destination buffer
+ * 
+ * @return 0 on success, or a non-zero SCROLL_ERR error code on failure
+ */
+MRC_ScrollStatus mrc_scroll_remove(MRC_Scroll *scroll, void *dest);
--- src/scroll.c
+++ src/scroll.c
@@ -1,12 +1,11 @@
 #include "../include/mrc/scroll.h"
+#include <string.h>
 
-static void wipe_scroll(MRC_Scroll *scroll)
-{
-  scroll -> data = NULL;
-  scroll -> span = 0;
-  scroll -> limit = 0;
-  scroll -> unit_size = 0;
-}
+static void wipe_scroll(MRC_Scroll *scroll);
+
+//=================================================================================================
+// METHODS
+//=================================================================================================
 
 MRC_ScrollStatus mrc_scroll_init(MRC_Scroll *scroll, size_t capacity, size_t u_size)
 {
@@ -36,4 +35,55 @@ MRC_ScrollStatus mrc_scroll_purge(MRC_Scroll *scroll)
   free(scroll -> data);
   wipe_scroll(scroll);
   return SCROLL_OK;
+}
+
+MRC_ScrollStatus mrc_scroll_push(MRC_Scroll *scroll, void *data)
+{
+  if (scroll == NULL || data == NULL) return SCROLL_ERR_ARG_INVALID;
+  if (scroll -> span >= scroll -> limit) {
+    size_t new_limit = scroll -> limit * 2;
+    if (new_limit > SIZE_MAX / scroll -> unit_size) return SCROLL_ERR_OVERFLOW;
+    void *new_buffer = realloc(scroll -> data, new_limit * scroll -> unit_size);
+    if (new_buffer == NULL) return SCROLL_ERR_OOM;
+    scroll -> data = new_buffer;
+    scroll -> limit = new_limit;
+  }
+  char *dest = (char *)scroll -> data + (scroll -> span * scroll -> unit_size);
+  memcpy(dest, data, scroll -> unit_size);
+  scroll -> span++;   // Don't forget to increment the span, pendejo
+  return SCROLL_OK;
+}
+
+MRC_ScrollStatus mrc_scroll_pop(MRC_Scroll *scroll, void *dest)
+{
+  if (scroll == NULL || dest == NULL) return  SCROLL_ERR_ARG_INVALID;
+  if (scroll -> span < 1) return SCROLL_ERR_BOUNDS;
+  char *src = (char *)scroll -> data + (scroll -> span * scroll -> unit_size);
+  memcpy(dest, src, scroll -> unit_size);
+  scroll -> span--;
+  return SCROLL_OK;
+}
+
+//=================================================================================================
+// STATIC METHODS
+//=================================================================================================
+
+/**
+ * @brief Sets all int values to zero and sets the data pointer to NULL
+ * 
+ * The purpose of this helper method is primarily during initialization
+ * and deletion of a scroll. The goal here is to ensure no junk data exists
+ * within the struct on init, and that we clear any potential hanging
+ * data/pointers on destruction. Maybe unnececssary, but cheap and safe.
+ * 
+ * @pre scroll must point to a valid MRC_Scroll struct
+ * 
+ * @param[in] scroll The address of the scroll to wipe
+ */
+static void wipe_scroll(MRC_Scroll *scroll)
+{
+  scroll -> data = NULL;
+  scroll -> span = 0;
+  scroll -> limit = 0;
+  scroll -> unit_size = 0;
 }
